3c3b352c30d903171457fddfa104b56705c109fb
busrayasar <19195145+busrayasar@users.noreply.github.com>
2021-01-01 16:14:20 +0300

    Add files via upload

---
+++ module usage/module.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  1 15:37:40 2021
+
+@author: busra
+"""
+#creating module (kütüphane-paket-modül)
+
+import  HesapModulu  as hm
+
+hm.yeni_maas(1000)
